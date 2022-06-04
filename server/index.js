fed272f07ee91cf2bed48cffd0716b5bb3a6bbd4
sametkoyuncu <sametkoyuncu@live.com>
2022-06-04 15:03:44 +0300

    End of mongodb part

--- server/index.js
+++ server/index.js
@@ -2,3 +2,32 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message))
+
+// It's a deprecated now. // No longer necessary
+// mongoose.set('useFindAndModify', false)
