214ed3bf05bf49e0f9219fbd06aee1e9218e9bad
sametkoyuncu <sametkoyuncu@live.com>
2022-06-22 13:45:32 +0300

    Add deploy configures

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({
+  baseURL: 'https://mern-memories-app-server.herokuapp.com/',
+})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
